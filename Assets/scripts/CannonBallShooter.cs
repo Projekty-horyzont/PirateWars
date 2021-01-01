155cb3de5868fdef056f3a581a6f7ba5fa6886f0
MushiX CSys <yxmusashixy@gmail.com>
2021-01-01 21:09:09 +0100

    Add Cannon ball shooting

---
+++ Assets/scripts/CannonBallShooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallShooter : MonoBehaviour
+{
+    public GameObject CannonBallPrefab;
+
+    public Vector3 SpawnPosition;
+    public Vector3 ShootDirection;
+
+    void Update()
+    {
+        if(!Input.GetKeyDown(KeyCode.Space)) return;
+
+        var ball = Instantiate(CannonBallPrefab);
+        ball.transform.position = transform.position + transform.rotation * SpawnPosition;
+
+        var rb = ball.GetComponent<Rigidbody>();
+        rb.velocity = transform.rotation * ShootDirection;
+    }
+}
