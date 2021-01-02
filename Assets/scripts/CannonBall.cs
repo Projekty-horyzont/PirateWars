23bbaa890131347eb83080e6e44506c2db0d163d
MushiX CSys <yxmusashixy@gmail.com>
2021-01-02 17:54:52 +0100

    add particles to terrain

---
+++ Assets/scripts/CannonBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public GameObject ParticleShoot; 
+
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var layerID = collision.collider.gameObject.layer;
+        var layerName = LayerMask.LayerToName(layerID);
+
+        GameObject particlesObject = null;
+
+        if(layerName == "Terrain")
+            particlesObject = ParticleShoot;
+
+        if(particlesObject != null)
+        {
+            var position = collision.contacts[0].point;
+            Instantiate(particlesObject, position, Quaternion.identity);
+        }
+    }
+}
